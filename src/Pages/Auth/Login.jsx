f8ba349869e499f491d495edbed06e79874effa0
CarlosSanchez4 <csanchez@evirom.com>
2025-03-18 22:23:10 +0100

    Inicio de sesión

--- src/Pages/Auth/Login.jsx
+++ src/Pages/Auth/Login.jsx
@@ -1,15 +1,34 @@
-import { useForm } from "react-hook-form";
+import { set, useForm } from "react-hook-form";
 import Header from "../../Components/Header";
+import api from "../../utils/api";
+import { useNavigate } from "react-router";
+import { toast } from "react-toastify";
+import { useContext } from "react";
+import { AppContext } from "../../Context/AppContext";
 
 const Login = () => {
+    const navigate = useNavigate();
+    const { setToken } = useContext(AppContext);
     const {
         register,
         handleSubmit,
         formState: { errors },
     } = useForm();
 
-    const onSubmit = (data) => {
-        console.log(data);
+    const onSubmit = async (data) => {
+        const response = await api("api/login", {
+            method: "POST",
+            body: JSON.stringify(data),
+        });
+        if (!response.error) {
+            localStorage.setItem("token", response.token);
+            setToken(response.token);
+            navigate("/");
+        } else {
+            toast.error(response.error, {
+                position: "top-center",
+            });
+        }
     };
     return (
         <>
@@ -39,7 +58,7 @@ const Login = () => {
                         <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
                             Contraseña
                         </label>
-                        <input {...register("password", { required: "Introduce la contraseña" })} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="nombre" type="text" placeholder="12345Ab_" />
+                        <input {...register("password", { required: "Introduce la contraseña" })} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="nombre" type="password" placeholder="12345Ab_" />
                         {errors.password && <p className="text-red-500 text-xs italic mt-2">{errors.password.message}</p>}
                     </div>
                     <button type="submit" className=" w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
